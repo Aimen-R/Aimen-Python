618c92202ffcfe42bf00f034ec38d9f5467b5b3b
Aimen R <aimen.rehan@education.nsw.gov.au>
2021-10-24 14:32:59 +1100

    Message

---
+++ 01HelloWorld.py
@@ -0,0 +1,9 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    01HelloWorld                                |")
+print("|    Name : Aimen Rehan                          |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+print("Hello World")
+
