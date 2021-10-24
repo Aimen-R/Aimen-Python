cdb0125337a601489b75a469e51220299d5ae768
Aimen R <aimen.rehan@education.nsw.gov.au>
2021-10-24 14:34:02 +1100

    commit

---
+++ 04GoodTeacher.py
@@ -0,0 +1,13 @@
+print(" ------------------------------------------------")
+print("|                                                |")
+print("|    04GoodTeacher                               |")
+print("|    Name : Aimen Rehan                          |")
+print("|    Version : 01                                |")
+print("|                                                |")
+print(" ------------------------------------------------")
+name = input("Teacher's name (try Mr Horan) ")
+x= "Mr Horan"
+if name == x:
+    print("You are lucky, he is a great teacher.")
+else:
+    print( (name) + " is an okay teacher")
